4f62d7bc377d7fccbbda0c5cee3276f8c94145bc
yuanrui <xdyrfree@gmail.com>
2025-01-08 22:34:48 +0800

    提交gitignore

--- intro/cpu.c
+++ intro/cpu.c
@@ -5,14 +5,13 @@
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
-	fprintf(stderr, "usage: cpu <string>\n");
-	exit(1);
+        fprintf(stderr, "usage: cpu <string>\n");
+        exit(1);
     }
     char *str = argv[1];
-
     while (1) {
-	printf("%s\n", str);
-	Spin(1);
+        printf("%s\n", str);
+        Spin(1);
     }
     return 0;
 }
